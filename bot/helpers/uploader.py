53f6fa0da2d2fa7a4246c8c480c7a0074918bec4
tarunjangid12 <146462019+tarunjangid12@users.noreply.github.com>
2023-12-21 05:07:57 +0530

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -69,7 +69,7 @@ class Uploader:
             )
             description = (
                 Config.VIDEO_DESCRIPTION
-                + "\nUploaded to YouTube with https://tx.me/youtubeitbot"
+                + "Thanks For Watching Our Video😊"
             )[:5000]
             if not Config.UPLOAD_MODE:
                 privacyStatus = "private"
